d4669031396b5f20161c664f86a551f016f10d02
Wind <loveswind@139.com>
2018-06-04 17:08:25 +0800

    First commit.

---
+++ app/src/main/java/com/loveswind/androidlearning/lovesweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.loveswind.androidlearning.lovesweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
